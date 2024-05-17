9cf39d31d58aaaad6f2e601bdf4722b345e37df9
Steven Burrell <sburrell23@gmail.com>
2024-05-17 13:18:45 -0600

    prompt working good for now

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 var admin = require("firebase-admin");
 var serviceAccount = require("../serviceAccountKey.json");
+const OpenAI = require('openai');
+const openai = new OpenAI({ apiKey: process.argv[2] });
 
 // Initialize Express
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 // Initialize Firebase
@@ -38,16 +41,55 @@ const neighborhoodNames = [
     "Lavender Fields"
 ];
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Define your routes here
+// Routes
 app.get('/all', async (req, res) => {
     const querySnapshot = await db.get();
     const citizens = querySnapshot.docs.map(doc => doc.data());
     res.send(citizens); 
 });
 
+app.get('/createCitizen', async (req, res) => {
+
+
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content:`create a random citizen for my city building game. 
+                The citizen is represented by a json object with the following properties:
+                {
+                    "firstName": "random name starting with the letter '${randChar()}' and ending with the letter '${randChar()}'",
+                    "lastName": "random last name starting with the letter '${randChar()}' and ending with the letter '${randChar()}'",
+                    "job": "random job starting with the letter '${randChar()}'",
+                    "age": random age between 1 - 100,
+                    "hobby": "random hobby starting with the letter '${randChar()}'",
+                    "favoriteFood": "random food starting with the letter '${randChar()}'"
+                }
+                Your job is to fill in the properties with crazy but realistic values.
+                `
+            },
+            { role: "user", content: ""},
+        ],
+        model: "gpt-4o",
+        response_format: { type: "json_object" },
+        temperature: 1.4
+    });
+    
+    var citizen = JSON.parse(completion.choices[0].message.content);
+    citizen.neighborhood = neighborhoodNames[Math.floor(Math.random() * neighborhoodNames.length)];
+    citizen.born = new Date().toLocaleString();
+
+    console.log(citizen);
+    res.send(citizen);
+
+});
+
+function randChar() {
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    return characters[randomIndex];
+}
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
